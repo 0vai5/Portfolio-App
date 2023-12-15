1bb2b4dbaf3f6bb0f79f221829a49549189bb346
Ovais Raza <rovais53@gmail.com>
2023-12-15 22:39:35 +0500

    done with island modal

---
+++ src/models/Island.jsx
@@ -0,0 +1,47 @@
+
+import { useRef, useEffect } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useThree, useFrame } from "@react-three/fiber";
+import { a } from "@react-spring/three";
+
+import IslandScene from "../assets/3d/island.glb";
+
+const Island = (props) => {
+  const islandRef = useRef();
+  const { nodes, materials } = useGLTF(IslandScene);
+  return (
+    <a.group {...props} ref={islandRef}>
+      <mesh
+        geometry={nodes.polySurface944_tree_body_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface945_tree1_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface946_tree2_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface947_tree1_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface948_tree_body_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface949_tree_body_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.pCube11_rocks1_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+    </a.group>
+  );
+};
+
+
+export default Island;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,43 @@
-import { React, Suspense } from 'react'
-import Canvas from '@react-three/fiber'
-import Loader from '../Components/Loader'
+import { React, Suspense } from "react";
+import Canvas from "@react-three/fiber";
+import Loader from "../Components/Loader";
+import Island from "../models/Island";
 
 const Home = () => {
-  return (
-    <section className='relative w-full h-screen'>
-        <Canvas 
-        className="w-full h-screen bg-transparent"
-        camera={{near: 0.1, far: 1000}}
-        >
-          <Suspense fallback= {<Loader />}>
-            <directionalLight />
-            <pointLight />
-            <ambientLight />
-            <spotLight/>
-            <hemisphereLight />
-          </Suspense>
-        </Canvas>
-    </section>
-  )
-}
+  const adjustIslandforScreenSize = () => {
+    let screenScale = null;
+    let screenPostion = [0, -6.5, 0.9];
+    let Rotation = [0.1, 4.7, 0.1]
 
-export default Home
+    if (window.innerWidth < 786) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPostion, Rotation];
+  };
+
+  const [IslandScale, IslandPosition, IslandRotation] = adjustIslandforScreenSize();
+  return (
+    <section className="relative w-full h-screen">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <pointLight />
+          <ambientLight />
+          <spotLight />
+          <hemisphereLight />
+
+          <Island scale={IslandScale} position={IslandPosition}
+          rotation = {IslandRotation} />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
